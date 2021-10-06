2a2a149b1b2eed657ad17f9e56138716ca0cabc6
yukiz97 <akayuki97@live.com>
2021-10-06 16:13:15 +0700

    update contact

--- contact.php
+++ contact.php
@@ -15,12 +15,21 @@
 	</section>
 
 	<section id="contact">
-		<div class="row">
-			<div class="col-md-4">
-
-			</div>
-			<div class="col-md-8">
-				
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<h5>Thời gian làm việc</h5>
+					<p>+ Từ thứ 2 đến thứ 6 hàng tuần</p>
+					<p>+ Thời gian làm việc: 8h - 16h</p>
+					<p>+ Thứ 7, chủ nhật và các ngày lễ nghỉ</p>
+					<p><b>SĐT:</b> (84-8) 6.299.7456</p>
+					<p><b>Fax:</b> (84-8) 6.264.6112</p>
+					<p><b>Mail:</b> contact@realtech.net.vn</p>
+					<p><b>Địa chỉ:</b> E7, Đường Nhật Tảo, Phường 7, Quận 11, Tp.Hồ Chí Minh</p>
+				</div>
+				<div class="col-md-8">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.6240204896153!2d106.64519631533419!3d10.763432262375334!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752e93dd7997c9%3A0x774e0ad5ed23b145!2zMzcgxJDGsOG7nW5nIHPhu5EgOSwgUGjGsOG7nW5nIDgsIFF14bqtbiAxMSwgVGjDoG5oIHBo4buRIEjhu5MgQ2jDrSBNaW5oLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1633511479336!5m2!1svi!2s" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+				</div>
 			</div>
 		</div>
 	</section>
